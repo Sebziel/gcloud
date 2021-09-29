f7c5a5a52fe9cfaa4a1f0687b4844a87392e6065
Sebziel <sebzie1996@gmail.com>
2021-09-29 22:16:14 +0000

    another bug fix

--- ELKStack/EK.sh
+++ ELKStack/EK.sh
@@ -6,8 +6,8 @@ sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install elasticsearch
 sleep 120
-IP_ADD =$(gcloud compute instances list --filter="NAME ~ ek-deployment" --format 'value(INTERNAL_IP)') |
-sudo sed -i "s|#network.host: 192.168.0.1|network.host: $IP_ADD|" /etc/elasticsearch/elasticsearch.yml
+VAR1=$(gcloud compute instances list --filter="NAME ~ ek-deployment" --format 'value(INTERNAL_IP)')
+sudo sed -i "s|#network.host: 192.168.0.1|network.host: $VAR1|" /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's|#discovery.seed_hosts: \["host1", "host2"\]|discovery.seed_hosts: []|' /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
