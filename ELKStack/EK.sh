e969ba317acc5b3bf878958466b56763c4792865
Sebziel <sebzie1996@gmail.com>
2021-09-29 16:44:34 +0000

    adding internal ip address as a variable

--- ELKStack/EK.sh
+++ ELKStack/EK.sh
@@ -5,7 +5,11 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install elasticsearch 
-sudo sed -i 's/#network.host: 192.168.0.1/network.host: 10.0.0.2/' /etc/elasticsearch/elasticsearch.yml
+IP_ADD =$(gcloud compute instances list --filter="NAME ~ ek-deployment" --format 'value(INTERNAL_IP)')
+sudo sed -i "s|#network.host: 192.168.0.1|network.host: $IP_ADD|" /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's|#discovery.seed_hosts: \["host1", "host2"\]|discovery.seed_hosts: []|' /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
+
+
+#gcloud compute instances list --filter="NAME ~ ek-deployment" --format 'value(INTERNAL_IP)'
