68af46fbe60bc8f1bd84622fe23ec904d3f15350
Sebziel <sebzie1996@gmail.com>
2021-09-29 21:47:12 +0000

    another attempt on ip addr bug

--- ELKStack/EK.sh
+++ ELKStack/EK.sh
@@ -5,12 +5,12 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install elasticsearch
-sleep 60
-IP_ADD =$(gcloud compute instances list --filter="NAME ~ ek-deployment" --format 'value(INTERNAL_IP)') | at now + 5 minutes
-sudo sed -i "s|#network.host: 192.168.0.1|network.host: $IP_ADD|" /etc/elasticsearch/elasticsearch.yml | at now + 5 minutes
+sleep 120
+IP_ADD =$(gcloud compute instances list --filter="NAME ~ ek-deployment" --format 'value(INTERNAL_IP)') |
+sudo sed -i "s|#network.host: 192.168.0.1|network.host: $IP_ADD|" /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's|#discovery.seed_hosts: \["host1", "host2"\]|discovery.seed_hosts: []|' /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch.service
-sudo systemctl start elasticsearch.service | at now + 5 minutes
+sudo systemctl start elasticsearch.service
 
 #Script is not working beacause the network host is not being added in elasticsearch.yml file, most probably because the account role is being addeed after the deployment, hence it's not possible to gather the ip address from the compute instances list 
 #gcloud compute instances list --filter="NAME ~ ek-deployment" --format 'value(INTERNAL_IP)'
